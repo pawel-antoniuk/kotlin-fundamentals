33ae5c2529670f05ea6fe143a18303739334cc2f
pawel <pawel@antoniuk.pl>
2022-08-28 23:10:00 +0200

    Remove Main file

--- src/main/kotlin/Main.kt
+++
@@ -1,7 +0,0 @@
-import kotlin.math.roundToInt
-import kotlin.random.Random
-
-fun main() {
-    for (i in 1..20)
-        println(List(10) { (Random.nextDouble(10.0) * 100).roundToInt() / 200.0 })
-}
